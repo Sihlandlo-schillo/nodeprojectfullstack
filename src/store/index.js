5ab0a74cc6d7de92adb6274d3bb69f1a7e4c48ae
KeiraKistensamy <Keirakist0987@gmail.com>
2024-08-19 14:05:11 +0200

    setup

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,58 @@
 import { createStore } from 'vuex'
+// import axios from 'axios'
+// import { toast } from 'vue3-toastify'
+// import 'vue3-toastify/dist/index.css'
+// import { applyToken } from '@/service/AuthenticatedUser.js'
+// import { useCookies } from 'vue3-cookies'
 
+// const {cookies} = useCookies()
+// const apiURL = 'http://localhost:3006/users/'
 export default createStore({
   state: {
+    users: null,
+    user: null,
+    products: null,
+    product: null
   },
   getters: {
   },
   mutations: {
+    // setUsers(state, value) {
+    //   state.users = value
+    // },
+    // setUser(state, value) {
+    //   state.user = value
+    // },
+    // setProducts(state, value) {
+    //   state.products = value
+    // },
+    // setRecentProducts(state, value) {
+    //   state.recentProducts = value
+    // },
+    // setProduct(state, value) {
+    //   state.product = value
+    // },
+
   },
   actions: {
+    // async recentProducts(context) {
+    //   try {
+    //     const { results, msg } = await (await axios.get(`${apiURL}product/recent`)).data
+    //     if (results) {
+    //       context.commit('setRecentProducts', results)
+    //     } else {
+    //       toast.error(`${msg}`, {
+    //         autoClose: 2000,
+    //         position: toast.POSITION.BOTTOM_CENTER
+    //       })
+    //     }
+    //   } catch (e) {
+    //     toast.error(`${e.message}`, {
+    //       autoClose: 2000,
+    //       position: toast.POSITION.BOTTOM_CENTER
+    //     })
+    //   }
+    // }
   },
   modules: {
   }
